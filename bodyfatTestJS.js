c3c36ae90bae4a2c84adf6a15533a232cfde8ebb
fmabees <fmabees@live.com>
2017-07-24 14:57:57 -0700

    Update bodyfatTestJS.js

--- bodyfatTestJS.js
+++ bodyfatTestJS.js
@@ -1,6 +1,6 @@
 function findBmi() {
     var height = document.getElementById("height").value;
     var weight = document.getElementById("weight").value;
-    var result = (weight / (height * height)) * 703;
-    document.getElementById("result").innerHTML = result;
+    var results = (weight / (height * height)) * 703;
+    document.getElementById("result").innerHTML = results;
 }
