691f1bfeea879bbdbd977012f6bfec5db46044d5
fmabees <fmabees@live.com>
2017-07-24 15:22:47 -0700

    Update bodyfatTestJS.js

--- bodyfatTestJS.js
+++ bodyfatTestJS.js
@@ -2,5 +2,12 @@ function findBmi() {
     var height = document.getElementById("height").value;
     var weight = document.getElementById("weight").value;
     var results = (weight / (height * height)) * 703;
+    var resultsStr = "";
     document.getElementById("result").innerHTML = Math.round(results * 100) / 100;
+    if (results < 18.5) {
+        resultsStr = "Underweight";
+    } else if (results >= 18.5 && results <= 24.9) {
+        resultsStr = "Healthy";
+    }
+    document.getElementById("result").innerHTML += resultsStr;
 }
