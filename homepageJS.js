a8967bf0a387d4cfee5eeae70227366d9557aeec
fmabees <fmabees@live.com>
2017-07-25 10:58:56 -0700

    Update homepageJS.js

--- homepageJS.js
+++ homepageJS.js
@@ -2,4 +2,9 @@ var quotes = [
     "You don't have to eat less; you just have to eat right.",
     "The best time to plant a tree was 20 years ago; the second best time is now.",
     "You are what you eat--so don't be Cheap, Easy or Fake."
-]; 
+];
+
+function getQuote() {
+    var chosenQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    document.getElementById("quote").innerHTML = "chosenQuote";
+}
