73803192f261c200309725f3fb0ed4f8b39aa109
Megha Raman <megha725@gmail.com>
2017-07-25 13:02:11 -0700

    Update StartJS.js

--- StartJS.js
+++ StartJS.js
@@ -1,7 +1,7 @@
 function surveyFunction() {
    var age = document.getElementById("age").value;
    var weight = document.getElementById("weight").value;
-   var height = document.getElemenetById("height").value;
+   var height = document.getElementById("height").value;
    var occupation = document.getElementById("occupation").value;
    var food = document.getElementById("food").value;
 }
