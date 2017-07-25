0cafdb8587b12919eefdbfb67a7f125bc0246697
fmabees <fmabees@live.com>
2017-07-25 10:40:39 -0700

    Update bodyFatTestJS.js

--- bodyFatTestJS.js
+++ bodyFatTestJS.js
@@ -15,6 +15,7 @@ function findBodyFat() {
     var result9 = result8 - result5;
     var leanBodyMass = +result6 + +result9;
     var bodyFatPercentage = ((weight - leanBodyMass) * 100) / weight;
+    document.getElementById("tableResult").style.visibility = "visible";
     document.getElementById("resultBodyFat").innerHTML = "Body Fat: " + Math.round(bodyFatPercentage * 1000) / 1000;
     document.getElementById("resultLeanBodyMass").innerHTML = "Lean Body Mass: " + Math.round(leanBodyMass * 1000) / 1000;
     var noteString = "*Note - These are approxiamtions. Do not base your diet plan on this. This will differ based on what you do, eg. Athlete, woman.\nIf you want a more accurate result, please try our body fat precentage calculator";
