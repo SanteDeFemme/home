4be5f1c5b3485c16f616e6d751dcc05e165e8a91
fmabees <fmabees@live.com>
2017-07-26 09:53:58 -0700

    Update StartJS.js

--- StartJS.js
+++ StartJS.js
@@ -1,15 +1,1 @@
-function GetParam(name)
-{
-  var start=location.search.indexOf("?"+name+"=");
-  if (start<0) start=location.search.indexOf("&"+name+"=");
-  if (start<0) return '';
-  start += name.length+2;
-  var end=location.search.indexOf("&",start)-1;
-  if (end<0) end=location.search.length;
-  var result='';
-  for(var i=start;i<=end;i++) {
-    var c=location.search.charAt(i);
-    result=result+(c=='+'?' ':c);
-  }
-  return unescape(result);
-}
+
