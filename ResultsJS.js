c2f0a54c4595dc7f6d0e87f9a1c6652d2ae7624d
fmabees <fmabees@live.com>
2017-07-26 09:11:35 -0700

    Update ResultsJS.js

--- ResultsJS.js
+++ ResultsJS.js
@@ -1,1 +1,19 @@
-document.getElementById("write").innerHTML = window.location.search;
+function getParams(){
+  var idx = document.URL.indexOf('?');
+  var params = new Array();
+  if (idx != -1) {
+    var pairs = document.URL.substring(idx+1, document.URL.length).split('&');
+    for (var i=0; i<pairs.length; i++){
+      nameVal = pairs[i].split('=');
+      params[nameVal[0]] = nameVal[1];
+    }
+  }
+  return params;
+}
+params = getParams();
+age = unescape(params["age"]);
+weight = unescape(params["weight"]);
+height = unescape(params["height"]);
+document.write("age = " + age + "<br>");
+document.write("weight = " + weight + "<br>");
+document.write("height = " + height + "<br>");
