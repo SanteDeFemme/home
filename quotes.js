1097a2fdf21177309c10da00abdeb0dd42bb1c3f
jwalamitra <jwala.a.mitra@gmail.com>
2017-07-27 14:11:46 -0700

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,6 +1,6 @@
 
-    var text = ["You don't have to eat less; you just have to eat right", "The best time to plant a tree was 20 years ago; the second best time is now", "You are what you eat--so don't be Cheap, Easy or Fake", "Healthy isn't a goal, it's a way of life", "When you feel like quitting, ask yourself why you started", "Failure is the only opportunity to begin again, only this time wisely", "The difference between try and triumph is just a little umph", "Today just get up and go, because yesterday, you said tomorrow", "Your body keeps and accurate journal regardless of what you write down", "It's not about being the best, it's about being better than you were yesterday"];
-    var counter = 5;
+    var text = ["1", "2", "3", "4", "5", "6"];
+    var counter = 6;
     var elem = document.getElementById("quote");
     setInterval(change, 1000);
     function change() {
