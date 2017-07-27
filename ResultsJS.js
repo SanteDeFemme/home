2f6f8ee8b3f0bdec8afb3574fa75151a6443bc8b
fmabees <fmabees@live.com>
2017-07-27 09:27:24 -0700

    Update ResultsJS.js

--- ResultsJS.js
+++ ResultsJS.js
@@ -18,8 +18,22 @@ height = +(heightF * 12) + +heightI;
 weight = unescape(params["weight"]);
 occupation = unescape(params["occupation"]);
 diet = unescape(params["food"]);
+bmi = (weight / (height * height)) * 703;
 document.write("age = " + age + "<br>");
 document.write("height = " + height + "<br>");
 document.write("diet = " + diet + "<br>");
 document.write("occupation = " + occupation + "<br>");
 document.write("weight = " + weight + "<br>");
+bmiStr = "";
+if (bmi < 18.5) {
+        bmiStr = "Underweight";
+    } else if (results >= 18.5 && results <= 24.9) {
+        resultsStr = "Healthy";
+        treatment = healthyTreatment;
+    } else if (results >= 25 && results <= 29.9) {
+        resultsStr = "Overweight";
+        treatment = overTreatment;
+    } else if (results => 30) {
+        resultsStr = "Obese";
+        treatment = obesityTreatment;
+    }
