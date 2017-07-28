47fd488ff37304e9793bb9deb0c60f1322a9b6d6
fmabees <fmabees@live.com>
2017-07-28 11:14:00 -0700

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -27,10 +27,10 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
+    this.isDeleting = false;
   this.period = parseInt(period, 10) || 2000;
   this.txt = '';
   this.tick();
-  this.isDeleting = false;
 };
 
 TxtRotate.prototype.tick = function() {
