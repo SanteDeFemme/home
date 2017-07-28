d5b8f69ef437c889803a4069765655f41b0c23e1
fmabees <fmabees@live.com>
2017-07-28 14:19:47 -0700

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -71,7 +71,7 @@ var j = 0;
      var period = elements[0].getAttribute('data-period');
    for (var i=0; i<10; i++) {
      if (toRotate) {
-             new TxtRotate(elements[0], quotes[i], period);
+             new TxtRotate(elements[0], quotes[j], period);
      }
    }
    // INJECT CSS
