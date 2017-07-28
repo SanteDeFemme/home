0bf3d97fcb2367e15af16b80d6888bdbf28e32dd
fmabees <fmabees@live.com>
2017-07-28 13:25:02 -0700

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -66,12 +66,13 @@
  
  function getQuote(){
    var elements = document.getElementsByClassName('txt-rotate');
-  for (var j=0; j<quotes.length; j++) {
+  var j = 0;
    for (var i=0; i<elements.length; i++) {
      var toRotate = elements[i].getAttribute('data-rotate');
      var period = elements[i].getAttribute('data-period');
      if (toRotate) {
              new TxtRotate(elements[i], quotes[j], period);
+      j++
      }
    }
    }
